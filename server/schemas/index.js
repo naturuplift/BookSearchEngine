7f06099230d7ef138c568302766c6ce4b88651a5
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2024-04-08 15:03:45 -0300

    create schemas

---
+++ server/schemas/index.js
@@ -0,0 +1,6 @@
+// import schemas
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+// Export typeDefs, resolvers to be used by main application
+module.exports = { typeDefs, resolvers };
