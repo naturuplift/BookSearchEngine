462461aa5d0237c0e3e6b39941293e1c589dfc35
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2024-04-09 14:27:38 -0300

    add App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,26 @@
+// import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/client';
+import apolloClient from './apolloClient'; // Ensure this points to your Apollo Client setup file
+
 import './App.css';
-import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={apolloClient}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<SearchBooks />} />
+          <Route path='/saved' element={<SavedBooks />} />
+          <Route path='*' element={<h1 className='display-2'>Wrong page!</h1>} />
+        </Routes>
+      </Router>
+    </ApolloProvider>
   );
 }
 
