14f16f82be2e01fdb2d5aa259f997fd0b03894bb
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2024-04-09 22:28:47 -0300

    update typeDefs.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,60 +4,60 @@ const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
-    Query: {
-        // Add query resolvers
-        me: async (parent, args, context) => {
-            if (context.user) {
-                return User.findOne({ _id: context.user._id }).populate('savedBooks');
-            }
-            throw new AuthenticationError('You need to be logged in!');
-        },
-    },
-    Mutation: {
-        // Add mutation resolvers
-        login: async (parent, { email, password }) => {
-            const user = await User.findOne({ email });
-            if (!user) {
-                throw new AuthenticationError('No user found with this email address');
-            }
-        
-            const correctPw = await user.isCorrectPassword(password);
-        
-            if (!correctPw) {
-                throw new AuthenticationError('Incorrect password');
-            }
-        
-            const token = signToken(user);
-            return { token, user };
-        },
-        addUser: async (parent, args) => {
-            const user = await User.create(args);
-            const token = signToken(user);
-            return { token, user };
-        },
-          saveBook: async (parent, { input }, context) => {
-            if (context.user) {
-              const updatedUser = await User.findOneAndUpdate(
-                { _id: context.user._id },
-                { $addToSet: { savedBooks: input } },
-                { new: true }
-              );
-              return updatedUser;
-            }
-            throw new AuthenticationError('You need to be logged in!');
-        },
-        removeBook: async (parent, { bookId }, context) => {
-            if (context.user) {
-              const updatedUser = await User.findOneAndUpdate(
-                { _id: context.user._id },
-                { $pull: { savedBooks: { bookId } } },
-                { new: true }
-              );
-              return updatedUser;
-            }
-            throw new AuthenticationError('You need to be logged in!');
-        },
+  Query: {
+      // Add query resolvers
+      me: async (parent, args, context) => {
+          if (context.user) {
+              return User.findOne({ _id: context.user._id }).populate('savedBooks');
+          }
+          throw new AuthenticationError('You need to be logged in!');
       },
+  },
+  Mutation: {
+    // Add mutation resolvers
+    login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+        if (!user) {
+            throw new AuthenticationError('No user found with this email address');
+        }
+    
+        const correctPasword = await user.isCorrectPassword(password);
+    
+        if (!correctPasword) {
+            throw new AuthenticationError('Incorrect password');
+        }
+    
+        const token = signToken(user);
+        return { token, user };
+    },
+    addUser: async (parent, args) => {
+        const user = await User.create(args);
+        const token = signToken(user);
+        return { token, user };
+    },
+    saveBook: async (parent, { input }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: input } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    removeBook: async (parent, { bookId }, context) => {
+        if (context.user) {
+          const updatedUser = await User.findOneAndUpdate(
+            { _id: context.user._id },
+            { $pull: { savedBooks: { bookId } } },
+            { new: true }
+          );
+          return updatedUser;
+        }
+        throw new AuthenticationError('You need to be logged in!');
+    },
+  },
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,7 +3,7 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type User {
-    _id: ID
+    _id: ID!
     username: String
     email: String
     bookCount: Int
@@ -27,14 +27,14 @@ type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
     saveBook(input: BookInput!): User
-    removeBook(bookId: String!): User
+    removeBook(bookId: ID!): User
   }
 
   input BookInput {
     authors: [String]
     description: String
     title: String
-    bookId: String
+    bookId: ID!
     image: String
     link: String
   }
