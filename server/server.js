ddb35a8b129c2fccc9e475a53b9e45022d6860b2
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2024-04-10 12:40:14 -0300

    refactor Apollo on top of express

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
+const { expressMiddleware } = require('@apollo/server/express4');
 // import schemas
 const { typeDefs, resolvers } = require('./schemas');
 // import mongoose connection
@@ -17,22 +18,26 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
  
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// provide client and build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
   
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  app.use('/graphql', expressMiddleware(server));
+  
+  // provide client and build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+  
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+
+
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`🌍 Now listening on localhost: ${PORT}!`);
