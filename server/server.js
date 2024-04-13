7272092b1d81af4d69ab0cd3f527e8b69d737331
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2024-04-13 00:12:50 -0300

    update server ApolloServer

--- server/server.js
+++ server/server.js
@@ -15,9 +15,6 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  persistedQueries: {
-    cache: new LruCache({ max: 1000 })
-  },
   context: authMiddleware,
 });
  
